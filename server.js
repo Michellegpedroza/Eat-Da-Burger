7cc2d45bb118a6a999fe4301278f20cc14e53635
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-11-18 20:41:24 -0800

    created server in server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+//Require npm packages
+const express = require(`express`)
+const { join } = require(`path`)
+
+//create the server
+const app = express()
+
+//Middleware
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+require('./routes')(app)
+
+//listen to a port
+app.listen(3000)
